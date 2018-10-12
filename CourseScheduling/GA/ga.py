1e15b2e63de46e8aaf0978f1d7d82fecae1c6cfc
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-10-12 19:08:38 +0530

    Working till parent selection

--- CourseScheduling/GA/ga.py
+++ CourseScheduling/GA/ga.py
@@ -9,7 +9,11 @@ from math import ceil, log10
 # professor	: 1-P
 # A chromosome is a combination of M genes (so that every course has a staff)
 
-def get_chromosome(num_days=5, num_slots=8, N=5, M=10, P=20):
+DEFAULT_M = 10
+DEFAULT_P = 20
+DEFAULT_N = 5
+
+def get_chromosome(num_days=5, num_slots=8, N=DEFAULT_N, M=DEFAULT_M, P=DEFAULT_P):
 	a = np.random.randint(1, num_days, M)
 	b = np.random.randint(1, num_slots, M)
 	c = np.random.randint(1, N, M)
@@ -18,48 +22,53 @@ def get_chromosome(num_days=5, num_slots=8, N=5, M=10, P=20):
 	chromosome = np.vstack((a,b,c,d,e))
 	return chromosome
 
-def get_fitness(chromosome, num_days=5, num_slots=8, N=5, M=10, P=20):
+def get_fitness(chromosome, num_days=5, num_slots=8, N=DEFAULT_N, M=DEFAULT_M, P=DEFAULT_P):
 	fitness = 0
-	# for i in range(1, P+1):		
 	free_profs = P - len(np.unique(chromosome[4]))
 	free_slots = 0
 	s = set()
 	for i in range(0, len(chromosome[0])):
-		# print(chromosome[0][i], chromosome[1][i])
-		s.add((chromosome[0][i], chromosome[1][i]))
-	# print(s)
+		s.add((chromosome[0,i], chromosome[1,i]))
 	free_slots = num_days*num_slots - len(s)
 	prof_clash = 0
 	venue_clash = 0
 	course_exceed_max_class = 0
 	course_changed_instructor = 0
-	for i in range(0, len(chromosome[0])):
-		for j in range(0, len(chromosome[0])):
-			if chromosome[0][i] == chromosome[0][j] and chromosome[1][i] == chromosome[1][j]:
+	for i in range(0, len(chromosome[:,0])):
+		for j in range(0, len(chromosome[:,0])):
+			if i == j:
+				continue
+			if chromosome[0,i] == chromosome[0,j] and chromosome[1,i] == chromosome[1,j]:
+				# print(' --> ', chromosome[0,i], chromosome[1,i], chromosome[2,i], chromosome[3,i], chromosome[4,i])
+				# print(' <-- ', chromosome[0,j], chromosome[1,j], chromosome[2,j], chromosome[3,j], chromosome[4,j])
+				# print('')
 				# stuff scheduled at same time
-				if chromosome[4][i] == chromosome[4][j]:
+				if chromosome[4,i] == chromosome[4,j]:
 					prof_clash += 1
-				if chromosome[2][i] == chromosome[2][j]:
+				if chromosome[2,i] == chromosome[2,j]:
 					venue_clash += 1
-			if chromosome[3][i] == chromosome[3][j]:
+			if chromosome[3,i] == chromosome[3,j]:
 				course_exceed_max_class += 1
-				if chromosome[4][i] != chromosome[4][i]:
+				if chromosome[4,i] != chromosome[4,i]:
 					course_changed_instructor += 1
 	CLASH_PENALTY = N*M*P
-	NORMALISING_FACTOR = 10**(ceil(log10(CLASH_PENALTY)) + 3)
+	NORMALISING_FACTOR = 10**(ceil(log10(CLASH_PENALTY)) )
 	clash_total = prof_clash + venue_clash + course_changed_instructor + course_exceed_max_class
+	# print(prof_clash, venue_clash, course_exceed_max_class, course_changed_instructor)
 	unfitness = free_profs + free_slots + clash_total * CLASH_PENALTY
-
+	# print(unfitness)
 	return NORMALISING_FACTOR * 1/unfitness
 
 def select_parents(population, fitness, num_select=5):
-	selected = np.empty((population[0].shape[1], num_select))
+	selected = []
 	for i in range(num_select):
-		most_fit = np.where(fitness == np.max(fitness))[0][0]
-		selected[i,:] = population[most_fit, :]
-		fitness[i] = -1
+		most_fit = np.where(fitness == np.max(fitness))
+		selected.append(population[most_fit[0][0]])
+		fitness[most_fit[0][0]] = -1
 	return selected
 
+# def do_crossovers(parents):
+	# 
 
 def main():
 	num_generations = 1
@@ -68,9 +77,10 @@ def main():
 	for i in range(50):
 		chx = get_chromosome()
 		population.append(chx)
+	# print(population)
 
 	for generation in range(num_generations):
-		fitness = np.empty(population.shape[1])
+		fitness = np.empty(len(population))
 		# Number of iterations for GA
 		# Steps:
 		# 	1) Calculate fitness
@@ -80,7 +90,11 @@ def main():
 		# 	5) Add new offsprings to the population
 		for i in range(len(population)):
 			fitness[i] = get_fitness(population[i])
-		print(select_parents(population, fitness))
+		# print(fitness)
+		selected = select_parents(population, fitness)
+		for i in selected:
+			print(get_fitness(i))
+		# print(fitness)
 
 if __name__ == '__main__':
 	main()
